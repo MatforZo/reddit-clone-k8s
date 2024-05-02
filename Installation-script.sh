436d119493ebce42aac3d94d105c019e852590ba
MatforZo <121509790+MatforZo@users.noreply.github.com>
2024-05-02 16:03:39 +0200

    Update Installation-script.sh
    added "-y" in docker installation

--- Installation-script.sh
+++ Installation-script.sh
@@ -27,7 +27,7 @@ if ! command_exists docker; then
     echo "Docker is not installed. Installing Docker..."
     # Add Docker's official GPG key:
     sudo apt-get update
-    sudo apt-get install ca-certificates curl
+    sudo apt-get install ca-certificates curl -y
     sudo install -m 0755 -d /etc/apt/keyrings
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -39,7 +39,7 @@ if ! command_exists docker; then
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
     # Install Docker
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
     if ! command_exists docker; then
         echo "Failed to install Docker."
         exit 1
@@ -166,3 +166,4 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
     echo "##############################################################################"
 else
     echo "Operation aborted."
+fi
